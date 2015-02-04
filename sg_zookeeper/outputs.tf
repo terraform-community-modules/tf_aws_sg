dd44a402c43f300c766530acfbd457729dbf95b5
Brandon Burton <brandon@inatree.org>
2015-02-04 00:50:40 +0000

    removing sg_common output

--- sg_zookeeper/outputs.tf
+++ sg_zookeeper/outputs.tf
@@ -1,8 +1,3 @@
-// Output ID of sg_common SG we made
-output "security_group_id_common" {
-  value = "${module.sg_common.security_group_id_common}"
-}
-
 // Output ID of sg_zookeeper SG we made
 output "security_group_id" {
   value = "${aws_security_group.main_security_group.id}"
