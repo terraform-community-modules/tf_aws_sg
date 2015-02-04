4baac720b1d656b7717d7bd3363bc90695509570
Brandon Burton <brandon@inatree.org>
2015-02-04 00:36:50 +0000

    fixing name for sg_common output in sg_zookeeper outputs

--- sg_zookeeper/outputs.tf
+++ sg_zookeeper/outputs.tf
@@ -1,6 +1,6 @@
 // Output ID of sg_common SG we made
 output "security_group_id_common" {
-  value = "${module.sg_common.main_security_group.id}"
+  value = "${module.sg_common.security_group_id_common}"
 }
 
 // Output ID of sg_zookeeper SG we made
