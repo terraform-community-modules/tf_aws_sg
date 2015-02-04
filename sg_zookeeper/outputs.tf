aeb97e5a6c9c1322f20973048af9890029771279
Brandon Burton <brandon@inatree.org>
2015-02-04 00:15:57 +0000

    fixing syntax error in outputs.tf

--- sg_zookeeper/outputs.tf
+++ sg_zookeeper/outputs.tf
@@ -1,6 +1,7 @@
 // Output ID of sg_common SG we made
 output "security_group_id_common" {
   value = "${module.sg_common.main_security_group.id}"
+}
 
 // Output ID of sg_zookeeper SG we made
 output "security_group_id" {
