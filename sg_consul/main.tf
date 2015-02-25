1ed1a651d55701003503f58794b84a9d22cac0b4
Brandon Burton <brandon@inatree.org>
2015-02-24 16:08:37 -0800

    Allowing all traffic inside SG for sg_consul

--- sg_consul/main.tf
+++ sg_consul/main.tf
@@ -17,6 +17,22 @@ resource "aws_security_group" "main_security_group" {
     description = "Security Group ${var.security_group_name}"
     vpc_id = "${var.vpc_id}"
 
+    // allows traffic from the SG itself for tcp
+    ingress {
+        from_port = 0
+        to_port = 65535
+        protocol = "tcp"
+        self = true
+    }
+
+    // allows traffic from the SG itself for udp
+    ingress {
+        from_port = 0
+        to_port = 65535
+        protocol = "udp"
+        self = true
+    }
+
     // allow traffic for TCP 22 (SSH)
     ingress {
         from_port = 22
