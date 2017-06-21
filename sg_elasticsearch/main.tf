a8b372c7b3d674df124cd342c94f85b31e147c27
Kamil Boratyński <kamil.boratynski@icloud.com>
2017-06-22 01:34:40 +0200

    [sg_elasticsearch]: removed `ssh`

--- sg_elasticsearch/main.tf
+++ sg_elasticsearch/main.tf
@@ -20,14 +20,6 @@ resource "aws_security_group" "main_security_group" {
     self      = true
   }
 
-  // allow traffic for TCP 22
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["${var.source_cidr_block}"]
-  }
-
   // allow traffic for TCP 9200 (REST Interface)
   ingress {
     from_port   = 9200
