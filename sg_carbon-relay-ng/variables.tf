66f829687e75e289c89b3a10aadd9e856321d2df
Kamil Boratyński <kamil.boratynski@icloud.com>
2017-06-28 13:20:48 +0200

    feature/list (#37)
    * [sg_zookeeper]: made `source_cidr_block` a list
    
    * [sg_zipkin]: made `source_cidr_block` a list
    
    * [sg_web]: made `source_cidr_block` a list
    
    * [sg_storm]: made `source_cidr_block` a list
    
    * [sg_ssh]: made `source_cidr_block` a list
    
    * [sg_redis]: made `source_cidr_block` a list
    
    * [sg_nomad]: made `source_cidr_block` a list
    
    * [sg_mysql]: made `source_cidr_block` a list
    
    * [sg_memcached]: made `source_cidr_block` a list
    
    * [sg_ldaps_only]: made `source_cidr_block` a list
    
    * [sg_ldap]: made `source_cidr_block` a list
    
    * [sg_kafka]: made `source_cidr_block` a list
    
    * [sg_elasticsearch]: made `source_cidr_block` a list
    
    * [sg_consul]: made `source_cidr_block` a list
    
    * [sg_cassandra]: made `source_cidr_block` a list
    
    * [sg_carbon-relay-ng]: made `source_cidr_block` a list

--- sg_carbon-relay-ng/variables.tf
+++ sg_carbon-relay-ng/variables.tf
@@ -9,5 +9,6 @@ variable "vpc_id" {
 
 variable "source_cidr_block" {
   description = "The source CIDR block to allow traffic from"
-  default     = "0.0.0.0/0"
+  default     = ["0.0.0.0/0"]
+  type        = "list"
 }
