1d91b62aebaf29d22bd89892d1cf2dcdecba9d0b
Kamil Boratyński <kamil.boratynski@icloud.com>
2017-06-30 22:05:26 +0200

    [sg_cassandra]: refactorized

--- sg_cassandra/main.tf
+++ sg_cassandra/main.tf
@@ -9,44 +9,40 @@ resource "aws_security_group" "main_security_group" {
   name        = "${var.security_group_name}"
   description = "Security Group ${var.security_group_name}"
   vpc_id      = "${var.vpc_id}"
+}
 
-  // allows traffic from the SG itself for tcp
-  ingress {
-    from_port = 0
-    to_port   = 65535
-    protocol  = "tcp"
-    self      = true
-  }
+// Allow any internal network flow.
+resource "aws_security_group_rule" "ingress_any_any_self" {
+  security_group_id = "${aws_security_group.main_security_group.id}"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "-1"
+  self              = true
+}
 
-  // allows traffic from the SG itself for udp
-  ingress {
-    from_port = 0
-    to_port   = 65535
-    protocol  = "udp"
-    self      = true
-  }
+// Allow TCP:9042 (Cassandra clients).
+resource "aws_security_group_rule" "ingress_tcp_9042_self" {
+  security_group_id = "${aws_security_group.main_security_group.id}"
+  from_port         = 9042
+  to_port           = 9042
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.source_cidr_block}"]
+}
 
-  // allow traffic for TCP 9042 (Cassandra clients)
-  ingress {
-    from_port   = 9042
-    to_port     = 9042
-    protocol    = "tcp"
-    cidr_blocks = ["${var.source_cidr_block}"]
-  }
+// Allow TCP:9160 (Cassandra Thrift clients)
+resource "aws_security_group_rule" "ingress_tcp_9160_self" {
+  security_group_id = "${aws_security_group.main_security_group.id}"
+  from_port         = 9160
+  to_port           = 9160
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.source_cidr_block}"]
+}
 
-  // allow traffic for TCP 9160 (Cassandra Thrift clients)
-  ingress {
-    from_port   = 9160
-    to_port     = 9160
-    protocol    = "tcp"
-    cidr_blocks = ["${var.source_cidr_block}"]
-  }
-
-  // allow traffic for TCP 7199 (JMX)
-  ingress {
-    from_port   = 7199
-    to_port     = 7199
-    protocol    = "tcp"
-    cidr_blocks = ["${var.source_cidr_block}"]
-  }
+// Allow TCP:7199 (JMX)
+resource "aws_security_group_rule" "ingress_tcp_7199_self" {
+  security_group_id = "${aws_security_group.main_security_group.id}"
+  from_port         = 7199
+  to_port           = 7199
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.source_cidr_block}"]
 }
