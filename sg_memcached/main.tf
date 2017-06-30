82c337b8dedf27c3a4ee63031e5916d068222d3a
Kamil Boratyński <kamil.boratynski@icloud.com>
2017-06-30 22:27:05 +0200

    [sg_memcached]: refactorized

--- sg_memcached/main.tf
+++ sg_memcached/main.tf
@@ -3,28 +3,22 @@ resource "aws_security_group" "main_security_group" {
   name        = "${var.security_group_name}"
   description = "Security Group ${var.security_group_name}"
   vpc_id      = "${var.vpc_id}"
+}
 
-  // allows traffic from the SG itself for tcp
-  ingress {
-    from_port = 0
-    to_port   = 65535
-    protocol  = "tcp"
-    self      = true
-  }
+// Allow any internal network flow.
+resource "aws_security_group_rule" "ingress_any_any_self" {
+  security_group_id = "${aws_security_group.main_security_group.id}"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "-1"
+  self              = true
+}
 
-  // allows traffic from the SG itself for udp
-  ingress {
-    from_port = 0
-    to_port   = 65535
-    protocol  = "udp"
-    self      = true
-  }
-
-  // allow traffic for TCP 11211
-  ingress {
-    from_port   = 11211
-    to_port     = 11211
-    protocol    = "tcp"
-    cidr_blocks = ["${var.source_cidr_block}"]
-  }
+// Allow TCP:11211 (Memcached)
+resource "aws_security_group_rule" "ingress_tcp_11211_cidr" {
+  security_group_id = "${aws_security_group.main_security_group.id}"
+  from_port         = 11211
+  to_port           = 11211
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.source_cidr_block}"]
 }
