2202b87db30dd06802919e08e6bb7630ea3a0827
Kamil Boratyński <kamil.boratynski@icloud.com>
2017-06-30 22:25:41 +0200

    [sg_ldaps_only]: refactorized

--- sg_ldaps_only/main.tf
+++ sg_ldaps_only/main.tf
@@ -3,12 +3,13 @@ resource "aws_security_group" "main_security_group" {
   name        = "${var.security_group_name}"
   description = "Security Group ${var.security_group_name}"
   vpc_id      = "${var.vpc_id}"
+}
 
-  // allow traffic for TCP 636
-  ingress {
-    from_port   = 636
-    to_port     = 636
-    protocol    = "tcp"
-    cidr_blocks = ["${var.source_cidr_block}"]
-  }
+// Allow TCP:636 (LDAPS)
+resource "aws_security_group_rule" "ingress_tcp_636_cidr" {
+  security_group_id = "${aws_security_group.main_security_group.id}"
+  from_port         = 636
+  to_port           = 636
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.source_cidr_block}"]
 }
