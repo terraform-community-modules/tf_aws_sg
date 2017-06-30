b6f618f5029cfbaf677b7061538e5d0efdc01226
Kamil Boratyński <kamil.boratynski@icloud.com>
2017-06-30 22:18:18 +0200

    [sg_https_only]: refactorized

--- sg_https_only/main.tf
+++ sg_https_only/main.tf
@@ -3,12 +3,13 @@ resource "aws_security_group" "main_security_group" {
   name        = "${var.security_group_name}"
   description = "Security Group ${var.security_group_name}"
   vpc_id      = "${var.vpc_id}"
+}
 
-  // allow traffic for TCP 443
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["${var.source_cidr_block}"]
-  }
+// Allow TCP:443 (HTTPS).
+resource "aws_security_group_rule" "ingress_tcp_443_cidr" {
+  security_group_id = "${aws_security_group.main_security_group.id}"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.source_cidr_block}"]
 }
