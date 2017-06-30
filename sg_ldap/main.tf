d369ccab26a6c18021707105be0bdbcec736b585
Kamil Boratyński <kamil.boratynski@icloud.com>
2017-06-30 22:24:48 +0200

    [sg_ldap]: refactorized

--- sg_ldap/main.tf
+++ sg_ldap/main.tf
@@ -3,28 +3,22 @@ resource "aws_security_group" "main_security_group" {
   name        = "${var.security_group_name}"
   description = "Security Group ${var.security_group_name}"
   vpc_id      = "${var.vpc_id}"
+}
 
-  // allows traffic from the SG itself for tcp
-  ingress {
-    from_port = 0
-    to_port   = 65535
-    protocol  = "tcp"
-    self      = true
-  }
+// Allow any internal network flow.
+resource "aws_security_group_rule" "ingress_any_any_self" {
+  security_group_id = "${aws_security_group.main_security_group.id}"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "-1"
+  self              = true
+}
 
-  // allows traffic from the SG itself for udp
-  ingress {
-    from_port = 0
-    to_port   = 65535
-    protocol  = "udp"
-    self      = true
-  }
-
-  // allow traffic for TCP 636 (LDAPS)
-  ingress {
-    from_port   = 636
-    to_port     = 636
-    protocol    = "tcp"
-    cidr_blocks = ["${var.source_cidr_block}"]
-  }
+// Allow TCP:636 (LDAPS)
+resource "aws_security_group_rule" "ingress_tcp_636_cidr" {
+  security_group_id = "${aws_security_group.main_security_group.id}"
+  from_port         = 636
+  to_port           = 636
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.source_cidr_block}"]
 }
