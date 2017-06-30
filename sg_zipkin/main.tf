03c810c269a1942a50344088cf70a78b8b8c7251
Kamil Boratyński <kamil.boratynski@icloud.com>
2017-06-30 22:49:59 +0200

    [sg_zipkin]: refactorized

--- sg_zipkin/main.tf
+++ sg_zipkin/main.tf
@@ -9,60 +9,58 @@ resource "aws_security_group" "main_security_group" {
   name        = "${var.security_group_name}"
   description = "Security Group ${var.security_group_name}"
   vpc_id      = "${var.vpc_id}"
+}
 
-  // allows traffic from the SG itself for tcp
-  ingress {
-    from_port = 0
-    to_port   = 65535
-    protocol  = "tcp"
-    self      = true
-  }
+// Allow any internal network flow.
+resource "aws_security_group_rule" "ingress_any_any_self" {
+  security_group_id = "${aws_security_group.main_security_group.id}"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "-1"
+  self              = true
+}
 
-  // allows traffic from the SG itself for udp
-  ingress {
-    from_port = 0
-    to_port   = 65535
-    protocol  = "udp"
-    self      = true
-  }
+// Allow TCP:9990 (Zipkin Admin port collector)
+resource "aws_security_group_rule" "ingress_tcp_9990_cidr" {
+  security_group_id = "${aws_security_group.main_security_group.id}"
+  from_port         = 9990
+  to_port           = 9990
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.source_cidr_block}"]
+}
 
-  // allow traffic for TCP 9990 (zipkin admin port collector)
-  ingress {
-    from_port   = 9990
-    to_port     = 9990
-    protocol    = "tcp"
-    cidr_blocks = ["${var.source_cidr_block}"]
-  }
+// Allow TCP:9901 (Zipkin Admin port query)
+resource "aws_security_group_rule" "ingress_tcp_9901_cidr" {
+  security_group_id = "${aws_security_group.main_security_group.id}"
+  from_port         = 9901
+  to_port           = 9901
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.source_cidr_block}"]
+}
 
-  // allow traffic for TCP 9901 (zipkin admin port query)
-  ingress {
-    from_port   = 9901
-    to_port     = 9901
-    protocol    = "tcp"
-    cidr_blocks = ["${var.source_cidr_block}"]
-  }
+// Allow TCP:9991 (Zipkin Admin port web)
+resource "aws_security_group_rule" "ingress_tcp_9991_cidr" {
+  security_group_id = "${aws_security_group.main_security_group.id}"
+  from_port         = 9991
+  to_port           = 9991
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.source_cidr_block}"]
+}
 
-  // allow traffic for UDP 9991 (zipkin admin port web)
-  ingress {
-    from_port   = 9991
-    to_port     = 9991
-    protocol    = "udp"
-    cidr_blocks = ["${var.source_cidr_block}"]
-  }
+// Allow TCP:9411 (Zipkin query port)
+resource "aws_security_group_rule" "ingress_tcp_9411_cidr" {
+  security_group_id = "${aws_security_group.main_security_group.id}"
+  from_port         = 9411
+  to_port           = 9411
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.source_cidr_block}"]
+}
 
-  // allow traffic for TCP 9411 (zipkin query port)
-  ingress {
-    from_port   = 9411
-    to_port     = 9411
-    protocol    = "tcp"
-    cidr_blocks = ["${var.source_cidr_block}"]
-  }
-
-  // allow traffic for TCP 8080 (zipkin web port)
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["${var.source_cidr_block}"]
-  }
+// Allow TCP:8080 (Zipkin web port)
+resource "aws_security_group_rule" "ingress_tcp_8080_cidr" {
+  security_group_id = "${aws_security_group.main_security_group.id}"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.source_cidr_block}"]
 }
