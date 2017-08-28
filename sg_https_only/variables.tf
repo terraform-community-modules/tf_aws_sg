a7d4197b7a6daafe94bc5b857a4bdb66cf808cff
Karthik Muralidharan <karthikmuralidharan@users.noreply.github.com>
2017-08-28 18:15:24 +0530

    fix source_cidr_block is expected to be a list than a string (#50)
    I kept receiving the error `aws_security_group_rule.ingress_tcp_443_cidr: cidr_blocks: should be a list`. This is because the variable `source_cidr_block` is a string than a list.
    
    This change allowed me to create the security group.

--- sg_https_only/variables.tf
+++ sg_https_only/variables.tf
@@ -9,7 +9,8 @@ variable "vpc_id" {
 
 variable "source_cidr_block" {
   description = "The source CIDR block to allow traffic from"
-  default     = "0.0.0.0/0"
+  default     = ["0.0.0.0/0"]
+  type        = "list"
 }
 
 variable "tags" {
