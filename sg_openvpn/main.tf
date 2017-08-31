e8d707f99f05eb4809237a797f006804077bab68
Jose Gaspar <jad.gaspar@gmail.com>
2017-08-31 10:30:51 +0100

    Added a openvpn security group

---
+++ sg_openvpn/main.tf
@@ -0,0 +1,30 @@
+//
+// Module: tf_aws_sg/sg_openvpn
+//
+//
+
+resource "aws_security_group" "main_security_group" {
+  name        = "${var.security_group_name}"
+  description = "tf-sg-${var.security_group_name}"
+  vpc_id      = "${var.vpc_id}"
+}
+
+// Allow any internal network flow.
+resource "aws_security_group_rule" "ingress_any_any_self" {
+  security_group_id = "${aws_security_group.main_security_group.id}"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "-1"
+  self              = true
+  type              = "ingress"
+}
+
+// Allow UDP:1194 (OpenVPN)
+resource "aws_security_group_rule" "ingress_udp_1194_cidr" {
+  security_group_id = "${aws_security_group.main_security_group.id}"
+  from_port         = 1194
+  to_port           = 1194
+  protocol          = "udp"
+  cidr_blocks       = "${var.source_cidr_block}"
+  type              = "ingress"
+}
